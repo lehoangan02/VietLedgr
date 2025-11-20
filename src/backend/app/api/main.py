fb641c9ca2875464c5e4fbde395974889196d9ab
NguyenHoang <nguyenislearning03@gmail.com>
2025-11-20 11:59:59 +0700

    FEAT: Add CRUD Operations and API Routers
    Product, Warehouse, Batch, Store

--- src/backend/app/api/main.py
+++ src/backend/app/api/main.py
@@ -1,7 +1,11 @@
-from app.api.routes import auth, user
+from app.api.routes import auth, user, product, store, warehouse, batch
 from fastapi import APIRouter
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router)
 api_router.include_router(user.router)
+api_router.include_router(product.router)
+api_router.include_router(store.router)
+api_router.include_router(warehouse.router)
+api_router.include_router(batch.router)
