646423728b486884fd5b34db567820ce88ee6966
hokhoa05 <khoaholtv0304@gmail.com>
2025-12-04 10:11:37 +0700

    Fix CashierLayout

--- src/frontend/app/(cashier)/layout.tsx
+++ src/frontend/app/(cashier)/layout.tsx
@@ -2,7 +2,7 @@ import { getCurrentUser } from "@/lib/fast-api/user";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
-export default async function ManagerLayout({ children }: { children: ReactNode}) {
+export default async function CashiserLayout({ children }: { children: ReactNode}) {
     const user = await getCurrentUser();
     if(!user) {
         redirect("/unauthorized");
