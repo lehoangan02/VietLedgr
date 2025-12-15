883f3823eb7e36452ba8b4d5811b39eab0741b00
hokhoa05 <khoaholtv0304@gmail.com>
2025-12-16 00:56:15 +0700

    bug-fix(user-admin): fix non-consistent of roleid and rolename, use alembic for migration

--- src/backend/app/api/deps.py
+++ src/backend/app/api/deps.py
@@ -5,7 +5,7 @@ import jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from pydantic import ValidationError
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 
 from app.core import security
 from app.core.config import settings
@@ -33,12 +33,13 @@ def get_current_user(session: SessionDep, token: TokenDep) -> User:
             token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
         )
         token_data = TokenPayload(**payload)
-    except (PyJWTError, ValidationError):
+    except (jwt.InvalidTokenError, ValidationError):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Could not validate credentials",
         )
-    user = session.get(User, token_data.sub)
+        
+    user = session.query(User).options(joinedload(User.role)).filter(User.user_id == token_data.sub).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
