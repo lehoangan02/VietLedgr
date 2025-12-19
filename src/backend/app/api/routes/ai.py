6dc2b7f102748aedcc5e886ff76efde949cc1476
NguyenHoang <nguyenislearning03@gmail.com>
2025-12-19 10:23:13 +0700

    Fix AI

--- src/backend/app/api/routes/ai.py
+++ src/backend/app/api/routes/ai.py
@@ -2,9 +2,10 @@ from app.services.ai_service import AIAgent, AIRequest
 from fastapi import APIRouter, Depends
 from app.core.config import settings
 from typing import Any
+import uuid
 
 from app.api.deps import SessionDep, get_current_user, CurrentUser
-from app.models import Batch, Expense, Warehouse, Store, Product, TransactionItem, TaxDetail
+from app.models import Batch, Expense, Warehouse, Store, Product, TransactionItem, TaxDetail, User
 
 
 router = APIRouter(
@@ -15,14 +16,14 @@ router = APIRouter(
 
 @router.post("/generate-report", )
 def generate_report(
-    current_user: CurrentUser,
+    # current_user: CurrentUser,
     session: SessionDep,
     ai_agent: AIAgent = Depends(lambda: AIAgent(api_key=settings.GEMINI_API_KEY))
 ) -> Any:
-    u_id = current_user.user_id
-    all_data = {}
+    user = session.query(User).filter(User.user_id == u_id).first()
+    all_stores = []
     # Gather data from the database
-    stores = session.query(Store).filter(Store.id == u_id).all()
+    stores = session.query(Store).filter(Store.id == user.store_id).all()
     for store in stores:
         warehouses = session.query(Warehouse).filter(Warehouse.store_id == store.id).all()
         expenses = session.query(Expense).filter(Expense.store_id == store.id).all()
@@ -38,27 +39,31 @@ def generate_report(
             for batch in batches:
                 batch_data = {}
                 product = session.query(Product).filter(Product.product_id == batch.product_id).first()
-                tax = session.query(TaxDetail).filter(TaxDetail.product_id == product.product_id).first()
+                # tax = session.query(TaxDetail).filter(TaxDetail.product_id == product.product_id).first()
                 transaction_items = session.query(TransactionItem).filter(TransactionItem.batch_id == batch.batch_id).all()
                 batch_data["product_name"] = product.name if product else "Unknown"
                 batch_data["stock"] = batch.stock
                 batch_data["sales_price"] = str(batch.sale_price)
                 batch_data["cost"] = str(batch.cost)
+                batch_data["warehouse_name"] = warehouse.name
+                batch_data["supplier"] = batch.supplier_name
                 if transaction_items:
                     for item in transaction_items:
                         batch_data["transaction"] = {}
                         batch_data["transaction"]["units_sold"] = item.quantity
                         batch_data["transaction"]["batch_revenue"] = str(item.quantity * batch.sale_price) - str(item.quantity * batch.cost)
-                        if tax:
-                            batch_data["transaction"]["tax"] = str(tax.tax_rate)
-                        else:
-                            batch_data["transaction"]["tax"] = "0"
+                        # if tax:
+                        #     batch_data["transaction"]["tax"] = str(tax.tax_rate)
+                        # else:
+                        #     batch_data["transaction"]["tax"] = "0"
                         batch_data["transaction"]["batch_date"] = item.transaction_date.strftime("%Y-%m-%d")
-                    all_data["batches"].append(batch_data)
-    print(all_data)
+                all_data["batches"].append(batch_data)
+        all_stores.append(all_data)
+
+    print(all_stores)
     # prompt = ai_agent.make_prompt(all_data)
     # ai_response = ai_agent.get_response(prompt)
     # code, report = ai_agent.parse_output(ai_response.response_text)
 
-    return all_data
+    return all_stores
 
